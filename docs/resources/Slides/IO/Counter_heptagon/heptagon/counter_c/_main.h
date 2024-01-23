72f3081a10390bb0e109cb5bb4d0cef3dfb0a838
rutujaekatpure <rattzzz07@gmail.com>
2024-01-23 17:38:23 +0530

    Lecture 4 & 5 and Lab 1 added

--- docs/resources/Slides/IO/Counter_heptagon/heptagon/counter_c/_main.h
+++
@@ -1,9 +0,0 @@
-/* --- Generated the 2/3/2023 at 17:18 --- */
-/* --- heptagon compiler, version 1.05.00 (compiled sat. jan. 15 3:31:18 CET 2022) --- */
-/* --- Command line: /usr/local/bin/heptc -target c -s main -hepts counter.ept --- */
-
-#ifndef _MAIN_H
-#define _MAIN_H
-
-#include "counter.h"
-#endif // _MAIN_H
